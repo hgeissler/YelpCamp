966df5f81cef91ecf1742b23940599e6f3d54c8e
Geißler Hannes <h.geissler@pke-de.com>
2020-07-01 18:05:29 +0200

    campgrounds functionality

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require('express')
 let app = express()
+app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.send('This will be the landing page')
+  res.render('landing')
+})
+
+app.get('/campgrounds', function (req, res) {
+  let campgrounds = [
+    {
+      name: 'Salmon Creek',
+      img:
+        'https://api.creativecommons.engineering/v1/thumbs/51ea6857-e202-4e84-8623-1ee864bcab3c',
+    },
+    {
+      name: 'Granite Hill',
+      img:
+        'https://api.creativecommons.engineering/v1/thumbs/208b70eb-8ffd-4236-be8e-8c46322afd44',
+    },
+    {
+      name: 'Mountain Goats Rest',
+      img:
+        'https://api.creativecommons.engineering/v1/thumbs/9f47e988-4c8d-4f4f-8dbd-66af44ff4131',
+    },
+  ]
+  res.render('campgrounds', { campgrounds: campgrounds })
 })
 
 app.listen('3000', function () {
