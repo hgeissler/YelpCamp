ff08c1241f18c2881db35b8341d216f52b4c47c9
Geißler Hannes <h.geissler@pke-de.com>
2020-07-02 09:18:50 +0200

    campgrounds styling

--- app.js
+++ app.js
@@ -21,6 +21,36 @@ let campgrounds = [
     image:
       'https://api.creativecommons.engineering/v1/thumbs/9f47e988-4c8d-4f4f-8dbd-66af44ff4131',
   },
+  {
+    name: 'Salmon Creek',
+    image:
+      'https://api.creativecommons.engineering/v1/thumbs/51ea6857-e202-4e84-8623-1ee864bcab3c',
+  },
+  {
+    name: 'Granite Hill',
+    image:
+      'https://api.creativecommons.engineering/v1/thumbs/208b70eb-8ffd-4236-be8e-8c46322afd44',
+  },
+  {
+    name: 'Mountain Goats Rest',
+    image:
+      'https://api.creativecommons.engineering/v1/thumbs/9f47e988-4c8d-4f4f-8dbd-66af44ff4131',
+  },
+  {
+    name: 'Salmon Creek',
+    image:
+      'https://api.creativecommons.engineering/v1/thumbs/51ea6857-e202-4e84-8623-1ee864bcab3c',
+  },
+  {
+    name: 'Granite Hill',
+    image:
+      'https://api.creativecommons.engineering/v1/thumbs/208b70eb-8ffd-4236-be8e-8c46322afd44',
+  },
+  {
+    name: 'Mountain Goats Rest',
+    image:
+      'https://api.creativecommons.engineering/v1/thumbs/9f47e988-4c8d-4f4f-8dbd-66af44ff4131',
+  },
 ]
 
 app.get('/', function (req, res) {
